6accd6def26e2dd0c023faf02593ffc087e9b568
glenchiridza <glenchiridz@gmail.com>
2022-07-20 13:05:40 +0200

    authview

--- task_base/views.py
+++ task_base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from .models import Goal
+
+
+class CustomLoginView(LoginView):
+    template_name = 'task_base/signin.html'
+    fields = '__all__'
+    redirect_authenticated_user =
 
 
 class GoalCreateView(generic.CreateView):
