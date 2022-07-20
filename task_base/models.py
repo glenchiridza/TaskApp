9e41d7582b9d045cbbbbaf6480aacf8a4921b4ab
glenchiridza <glenchiridz@gmail.com>
2022-07-20 12:01:17 +0200

    goal model

--- task_base/models.py
+++ task_base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             null=True, blank=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True, blank=True)
+    is_complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['complete']
+
+    def __str__(self):
+        return self.title
